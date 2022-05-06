8c8bc8295f6e088abb2c8bbe784c970e5ec2bf70
LeppoXena <francesco.lepore@xenagames.com>
2022-05-06 17:33:03 +0200

    f**king points added. :)

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 // identifico dove fare apparire e scomparire l'html: "hamburger-menu" di default none, "hamburger-menu active" in display block.
 
-let menuHidden = document.querySelector("hamburger-menu");
+let menuHidden = document.querySelector(".hamburger-menu");
 
 // identifico i due punti dell'evento click: i.fas.fa-bars e a.close
 
-const menuOpen = document.querySelector("fa-bars");
+const menuOpen = document.querySelector(".fa-bars");
 
-const menuClose = document.querySelector("close");
+const menuClose = document.querySelector(".close");
 
 // genero un evento click con funnzione apri
 
